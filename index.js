24930107d5419d44bf53b6c0a98b64f84030566d
Eman Al-zariey <eman_alzariey@yahoo.com>
2019-10-22 06:23:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 // Write your solution in this file!
 
 
-const obj = { foo: "bar", key: "value" };
+// const obj = { foo: "bar", key: "value" };
 
-function updateDriverWithKeyAndValue(obj, key, value){
+function updateDriverWithKeyAndValue(driver, key, value){
     //doesn't change driver and returns newDriver
-    const newObj = Object.assign({}, obj)
+    const newObj = Object.assign({}, driver)
 
     newObj[key] = value; // has an updated value for the key passed in.
 
     return newObj;
 }
 
-function destructivelyUpdateDriverWithKeyAndValue(obj, key, value){
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
     // changes driver and returns newDriver//take key from original and overwrite it
-    obj[key] = value;
+    driver[key] = value;
 
-    return obj; //return new
+    return driver; //return new
 
 }
 
 
-function deleteFromDriverByKey(obj, key) {
+function deleteFromDriverByKey(driver, key) {
     // don't change the driver
-    const newObj = Object.assign({}, obj)
+    const newObj = Object.assign({}, driver)
     delete newObj.key; // or delete newObj[key];
 
 }
 
-function destructivelyDeleteFromDriverByKey(obj, key){
+function destructivelyDeleteFromDriverByKey(driver, key){
     // should have effect on the driver
-    delete object.key;
+    delete driver.key;
 }
